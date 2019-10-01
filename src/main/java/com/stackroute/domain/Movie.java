f84777450f2bd5c5179cf14e0615f9ee50ceb237
ganemouni <gkorada@student.nitw.ac.in>
2019-10-01 22:49:21 +0530

    first commit

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,4 +1,14 @@
 package com.stackroute.domain;
 
-public class Movie extends Actor {
+public class Movie {
+    Actor actor;
+
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+    public void display(){
+        System.out.println("Name of the actor: "+ this.actor.name);
+        System.out.println("Gender of the actor: "+ this.actor.gender);
+        System.out.println("Age of the actor: "+ this.actor.age);
+    }
 }
--- src/main/java/com/stackroute/main.java
+++ src/main/java/com/stackroute/main.java
@@ -1,6 +1,7 @@
 package com.stackroute;
 
 import com.stackroute.domain.Actor;
+import com.stackroute.domain.Movie;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
@@ -9,11 +10,20 @@ import org.springframework.core.io.ClassPathResource;
 
 public class main {
     public static void main(String[] args){
-//    ApplicationContext context= new ClassPathXmlApplicationContext("bean.xml");
-        BeanFactory factory= new XmlBeanFactory(new ClassPathResource("bean.xml"));
+    ApplicationContext context= new ClassPathXmlApplicationContext("bean.xml");
+//        BeanFactory factory= new XmlBeanFactory(new ClassPathResource("bean.xml"));
 
-    Actor a1=(Actor)factory.getBean("Actor1");
-    a1.display();
+    Movie m1=(Movie)context.getBean("movieA");
+        Movie m2=(Movie) context.getBean("movieB");
+    m1.display();
+    m2.display();
+
+        if (m1 == m2) {
+            System.out.println(m1==m2);
+        }
+        else{
+            System.out.println("not equal");
+        }
     }
 
 }
